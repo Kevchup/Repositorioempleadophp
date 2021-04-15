f689c5bf6d1cdd83cb41da6ebe7d97d486c91fa8
Kevchup <81330494+Kevchup@users.noreply.github.com>
2021-04-14 22:39:07 -0500

    linea cambio

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $vehiculo = "SELECT * FROM dvehiculo";
 <p>Cedula:<input type="text" name="cedula" pattern="[0-9]{6,10}" title="La cedula debe ser solo números y tener una longitud máxima de 10 caracteres" required></p>
 <p>Nombre:<input type="text" name="nombre" pattern="[a-zA-Z- ]{1,50}" title="En el nombre solo debe poner letras y tener una longitud máxima de 50 caracteres" required></p>
 <p>Marca:<input type="text" name="marca" pattern="[a-zA-Z]{1,30}" title="La marca debe ser solo letras y tener una longitud máxima de 30 caracteres" required></p>
-<p>Linea:<input type="text" name="linea" pattern="[a-zA-Z][a-zA-Z0-9-_\.]{1,30}" title="" required></p>
+<p>Linea:<input type="text" name="linea" pattern="[a-zA-Z- ][a-zA-Z0-9-_\.]{1,30}" title="" required></p>
 <p>Modelo:<input type="text" name="modelo" pattern="[0-9]{1,30}" title="La marca debe ser solo números y tener una longitud máxima de 30 caracteres " required></p>
 <p>Valor:<input type="text" name="valor" pattern="[0-9]{1,30}" title="El valor debe ser solo números y tener una longitud máxima de 30 caracteres " required></p>
 <p><input type="submit" name="submit" /></p>
